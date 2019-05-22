7e6a318e644a07e33839c7043277f6c83ab829de
lzhaoGitHub <zhaol192@outlook.com>
2019-05-22 09:34:14 +0800

    update pmatemask  ;

--- ui/app/pages/create-account/import-account/index.js
+++ ui/app/pages/create-account/import-account/index.js
@@ -39,17 +39,17 @@ AccountImportSubview.prototype.render = function () {
 
       h('.new-account-import-disclaimer', [
         h('span', this.context.t('importAccountMsg')),
-        h('span', {
-          style: {
-            cursor: 'pointer',
-            textDecoration: 'underline',
-          },
-          onClick: () => {
-            global.platform.openWindow({
-              url: 'https://metamask.zendesk.com/hc/en-us/articles/360015289932',
-            })
-          },
-        }, this.context.t('here')),
+        // h('span', {
+        //   style: {
+        //     cursor: 'pointer',
+        //     textDecoration: 'underline',
+        //   },
+        //   onClick: () => {
+        //     global.platform.openWindow({
+        //       url: 'https://metamask.zendesk.com/hc/en-us/articles/360015289932',
+        //     })
+        //   },
+        // }, this.context.t('here')),
       ]),
 
       h('div.new-account-import-form__select-section', [
